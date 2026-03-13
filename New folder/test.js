5fa685e14f713510dcbf9926160950ac4a31ec36
Kartik-hub16 <kartikkohad16@gmail.com>
2026-03-13 19:16:09 +0530

    Add files via upload

---
+++ New folder/test.js
@@ -0,0 +1,4 @@
+console.log("Test file - checking environment...");
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("GROQ_API_KEY exists:", !!process.env.GROQ_API_KEY);
+console.log("Current directory:", process.cwd());
